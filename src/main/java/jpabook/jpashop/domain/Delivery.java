43bd590c4fffb549599347a460b074aace6c93dd
sangwoo lee <lswoo0705@gmail.com>
2024-01-31 18:25:50 +0900

    Entity Design Points

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -14,9 +14,12 @@ public class Delivery {
     @GeneratedValue
     @Column(name = "delivery_id")
     private Long id;
+
     @OneToOne(mappedBy = "delivery")
     private Order order;
+
     private Address address;
+
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
 }
