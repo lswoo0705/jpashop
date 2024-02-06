6c3455c407a900fe1a5888ef739edd8cdddf227d
sangwoo lee <lswoo0705@gmail.com>
2024-02-06 19:03:08 +0900

    ItemEntity

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.Item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,9 +19,29 @@ public abstract class Item {
     @GeneratedValue
     @Column(name = "item_id")
     private Long id;
+
     private String name;
+
     private int price;
+
     private int stockQuantity;
+
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    // 재고 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 더 필요합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
 }
