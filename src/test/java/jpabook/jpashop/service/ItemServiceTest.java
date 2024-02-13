aab56538364db398e5c61cea0a5412c1b3212a91
sangwoo lee <lswoo0705@gmail.com>
2024-02-13 20:39:59 +0900

    OrderServiceTest

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -5,10 +5,8 @@ import jpabook.jpashop.domain.Item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -16,7 +14,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 class ItemServiceTest {
